feaec3a6e5fd8e1f9a1bfcd5b8e5fb2e8b9965f9
Friedrich Tuttas <friedrich.tuttas@ifr.uni-stuttgart.de>
2025-02-26 19:19:32 +0100

    edit function vor transforming keplerian elements

---
+++ +smu/+frames/cartesianFromKepler.m
@@ -0,0 +1,99 @@
+function [position__m, velocity__m_per_s] = cartesianFromKepler(semi_major_axis__m, ...
+                                                        eccentricity, ...
+                                                        inclination__rad, ...
+                                                        right_ascension_of_ascending_node__rad, ...
+                                                        argument_of_periapsis__rad, ...
+                                                        true_anomaly__rad, ...
+                                                        gravitational_parameter__m3_per_s2)
+%% cartesianFromKepler - Translates Kepler Elements to Cartesian Position and Velocity Vectors
+%
+%   Inputs:
+%       gravitational_parameter_m3_per_s2: Gravitational parameter of the central body
+%       semi_major_axis__m: semi-major axis of the orbit
+%       eccentricity: eccentricity of the orbit
+%       inclination__rad: inclination of the orbit
+%       right_ascension_of_ascending_node__rad: right ascension of the orbit's ascending node 
+%       argument_of_periapsis__rad: argument of the orbit's periapsis
+%       true_anomaly__rad: true anomaly of current position
+%   
+%   Outputs:
+%       position__m:   position vector from the central body to spacecraft
+%       velocity__m_per_s: velocity vector of the spacecraft
+%
+%% References:
+% [1] R. R. Bate, D. D. Mueller, and J. E. White, Fundamentals of Astrodynamics, 1st ed. in Dover books on astronomy. New York: Dover Publications, 1971.
+
+
+arguments
+    semi_major_axis__m (1,:) {mustBeNumeric, mustBeReal, mustBeNonzero}
+    eccentricity (1,:) {mustBeNumeric, mustBeReal, mustBePositive, mustNotBeOne, smu.argumentValidation.mustBeEqualLength(semi_major_axis__m, eccentricity, 2, 2)}
+    inclination__rad (1,:) {mustBeNumeric, mustBeReal, smu.argumentValidation.mustBeEqualLength(semi_major_axis__m, inclination__rad, 2, 2)}
+    right_ascension_of_ascending_node__rad (1,:) {mustBeNumeric, mustBeReal, smu.argumentValidation.mustBeEqualLength(semi_major_axis__m, right_ascension_of_ascending_node__rad, 2, 2)}
+    argument_of_periapsis__rad (1,:) {mustBeNumeric, mustBeReal, smu.argumentValidation.mustBeEqualLength(semi_major_axis__m, argument_of_periapsis__rad, 2, 2)}
+    true_anomaly__rad (1,:) {mustBeNumeric, mustBeReal, smu.argumentValidation.mustBeEqualLength(semi_major_axis__m, true_anomaly__rad, 2, 2)}
+    gravitational_parameter__m3_per_s2 (1,:) {mustBeNumeric, mustBeReal, mustBePositive, smu.argumentValidation.mustBeEqualLength(semi_major_axis__m, gravitational_parameter__m3_per_s2, 2, 2)}
+end
+
+%% Abbreviations
+
+% shape of the orbit
+a = semi_major_axis__m;
+ecc = eccentricity;
+% orientation of the orbit
+inc = inclination__rad;
+RAAN = right_ascension_of_ascending_node__rad;
+omega = argument_of_periapsis__rad;
+% position of the spacecraft
+nu = true_anomaly__rad;
+% gravitational parameter
+mu = gravitational_parameter__m3_per_s2;
+
+%% Algorithm from [1]
+
+% Calculations in perifocal frame
+% Position
+p = a * (1 - ecc^2);
+r_mag = p / (1 + ecc * cos(nu));
+
+pos_P__m = r_mag * [cos(nu); sin(nu); 0];
+
+% Velocity
+vel_P__m_per_s = sqrt(mu / p) * [-sin(nu); ecc + cos(nu); 0];
+
+% Determine direction cosine matrix from inc, RAAN, and omega 
+DCM = zeros(3,3);
+
+cInc = cos(inc);
+sInc = sin(inc);
+cRaan = cos(RAAN);
+sRaan = sin(RAAN);
+cOmega = cos(omega);
+sOmega = sin(omega);
+
+DCM(1,1) = cRaan * cOmega - sRaan * sOmega * cInc;
+DCM(1,2) = -cRaan * sOmega - sRaan * cOmega * cInc;
+DCM(1,3) = sRaan * sInc;
+
+DCM(2,1) = sRaan * cOmega + cRaan * sOmega * cInc;
+DCM(2,2) = -sRaan * sOmega + cRaan * cOmega * cInc;
+DCM(2,3) = -cRaan * sInc;
+
+DCM(3,1) = sOmega * sInc;
+DCM(3,2) = cOmega * sInc;
+DCM(3,3) = cInc;
+
+% Transformation to equatorial frame
+position__m = DCM * pos_P__m;
+velocity__m_per_s = DCM * vel_P__m_per_s;
+
+end
+
+%% Custom validation functions
+
+function mustNotBeOne(val)
+    if any(val == 1)
+        eid = "mustNotBeOne:isOne";
+        msg = "Argument must not contain values equal to 1.";
+        error(eid,msg)
+    end
+end
