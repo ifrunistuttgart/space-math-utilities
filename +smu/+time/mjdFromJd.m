dee41f6f49fab68de093375eafebae8eb8dbc622
Friedrich Tuttas <53369972+fritut08@users.noreply.github.com>
2025-03-19 11:59:45 +0100

    adjust variable name

--- +smu/+time/mjdFromJd.m
+++ +smu/+time/mjdFromJd.m
@@ -1,4 +1,4 @@
-function time_mjd__days = mjdFromJd(time__jd_days)
+function time_mjd__days = mjdFromJd(time_jd__days)
 % mjdFromJd - calculate modified julian date from julian date
 %   both given in days. 
 %   MJD is the number of days since midnight on November 17 1858
@@ -7,7 +7,7 @@ function time_mjd__days = mjdFromJd(time__jd_days)
 %% References: [1] O. Montenbruck and E. Gill, Satellite Orbits. Berlin, 
 %                  Heidelberg: Springer Berlin Heidelberg, 2000. doi: 10.1007/978-3-642-58351-3.
 
-    time_mjd__days = time__jd_days - 2400000.5;
+    time_mjd__days = time_jd__days - 2400000.5;
 
 end
 
